5ca08c505ae8d918837ded8c52b539bd51a6e639
Jakub Chełchowski <jakubchelchowski@gmail.com>
2021-12-26 22:01:05 +0100

    GW-form_validatons_added

--- glosariusz/src/panel/ui/words/WordCreate.js
+++ glosariusz/src/panel/ui/words/WordCreate.js
@@ -1,18 +1,19 @@
 import { ArrayInput, BooleanInput, Create, SimpleForm, SimpleFormIterator, TextInput } from 'react-admin'; 
+import validators from './utils/validators';
 
 const WordCreate = (props) => {
 	return (
 		<Create {...props} title='Add word to the dictionary'>
 			<SimpleForm>
-				<TextInput source='polishWord' required={true}/>
+				<TextInput source='polishWord' validate={validators.validatePolishWord}/>
 				<ArrayInput source='synonyms'>
 					<SimpleFormIterator>
-						<TextInput source='singularForm' label='Singular form'/>
-						<TextInput source='pluralCountable' label='Plural Countable Form'/>
-						<TextInput source='pluralUncountable' label='Plural Uncountable Form'/>
+						<TextInput source='singularForm' label='Singular form' validate={validators.validateSynonym}/>
+						<TextInput source='pluralCountable' label='Plural Countable Form' validate={validators.validateSynonym}/>
+						<TextInput source='pluralUncountable' label='Plural Uncountable Form' validate={validators.validateSynonym}/>
 					</SimpleFormIterator>
 				</ArrayInput>
-				<TextInput source='definition' multiline={true} required={true}/>
+				<TextInput source='definition' multiline={true} validate={validators.validateDefinition}/>
 				<BooleanInput label="Reference" source="reference" />
 			</SimpleForm>
 		</Create>
--- glosariusz/src/panel/ui/words/WordEdit.js
+++ glosariusz/src/panel/ui/words/WordEdit.js
@@ -1,19 +1,20 @@
 import { ArrayInput, BooleanInput, Edit, SimpleForm, SimpleFormIterator, TextInput } from 'react-admin'; 
+import validators from './utils/validators';
 
 const WordEdit = (props) => {
 	return (
 		<Edit {...props} title='Edit existing word'>
 			<SimpleForm>
-				<TextInput disabled source='id' required={true}/>
-				<TextInput source='polishWord' required={true}/>
+				<TextInput disabled source='id'/>
+				<TextInput source='polishWord' validate={validators.validatePolishWord}/>
 				<ArrayInput source='synonyms'>
 					<SimpleFormIterator>
-						<TextInput source='singularForm' label='Singular form'/>
-						<TextInput source='pluralCountable' label='Plural Countable Form'/>
-						<TextInput source='pluralUncountable' label='Plural Uncountable Form'/>
+						<TextInput source='singularForm' label='Singular form' validate={validators.validateSynonym}/>
+						<TextInput source='pluralCountable' label='Plural Countable Form' validate={validators.validateSynonym}/>
+						<TextInput source='pluralUncountable' label='Plural Uncountable Form' validate={validators.validateSynonym}/>
 					</SimpleFormIterator>
 				</ArrayInput>
-				<TextInput source='definition' multiline={true} required={true}/>
+				<TextInput source='definition' multiline={true} validate={validators.validateDefinition}/>
 				<BooleanInput label="Reference" source="reference" />
 			</SimpleForm>
 		</Edit>
