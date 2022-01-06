c1baba35e11e8573b305a1cc7d937e14f76dbc65
Wiktor Morawski <wiktormorawski@MacBook-Air-Wiktor.local>
2022-01-06 03:52:45 +0100

    GW_modified_pagination_with_no_words_found

--- glosariusz/src/main/ducks/reducers/apiCallsReducer.js
+++ glosariusz/src/main/ducks/reducers/apiCallsReducer.js
@@ -1,6 +1,6 @@
 import { PAGINATION_GET_SUCCESS, SET_ERROR, SET_LOADING } from '../actions/types';
 const init_state = {
-	isLoading: false,
+	isLoading: true,
 	isError: false
 };
 const apiCallsReducer = (state=init_state, action) => {
